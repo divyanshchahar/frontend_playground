e9c4becd70003d5c2cdaa3d5c16bfdbc38ce8976
Divyansh Chahar <divyanshchahar@outlook.com>
2023-02-26 14:36:36 +0530

    integrated new componenet

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Profile from "./components/Profile";
 
 function App() {
-  return <div>Blank App</div>;
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
