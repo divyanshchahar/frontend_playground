7b127ca0e202470d7634a0723e64b27a796ac3ed
Divyansh Chahar <divyanshchahar@outlook.com>
2023-02-26 15:11:10 +0530

    integrated new functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Profile from "./components/Profile";
+import ActionButtons from "./components/ActionButtons";
 
 function App() {
   return (
     <div>
       <Profile />
+      <ActionButtons />
     </div>
   );
 }
