6f6bc63bf53f8877ea33f0c355db7cba83c9b4a4
Divyansh Chahar <divyanshchahar@outlook.com>
2023-02-26 15:08:39 +0530

    added logout reducer

--- src/features/users.js
+++ src/features/users.js
@@ -1,16 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialUSerState = {
+  name: "John Doe",
+  age: 24,
+  email: "johndoe@nomail.com",
+};
+
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-    value: { name: "John Doe", age: 24, email: "johndoe@nomail.com" },
+    value: initialUSerState,
   },
   reducers: {
     login: (state, action) => {
-      state.vale = action.payload;
+      state.value = action.payload;
+    },
+    logout: (state) => {
+      state.value = initialUSerState;
     },
   },
 });
 
-export const { login } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 export default userSlice.reducer;
