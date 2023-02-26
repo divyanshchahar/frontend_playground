c1ea985e0982baa1244786c555f33f97e1f4e91a
Divyansh Chahar <divyanshchahar@outlook.com>
2023-02-26 21:08:41 +0530

    changed todos slice

--- src/features/todos.js
+++ src/features/todos.js
@@ -1,28 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialTodos = [{ _id: 0, name: "first todod", compltetd: false }];
+const initialTodos = [
+  { _id: 0, name: "first todod", completed: false },
+  { _id: 1, name: "second todod", completed: false },
+];
 
 export const todosSlice = createSlice({
   name: "todos",
   initialState: { value: initialTodos },
   reducers: {
     addTodo: (state, action) => {
-      state.value.push(action.payload);
-    },
-    toggleTodo: (state, action) => {
-      state.value = state.value.map((item) => {
-        if (item.id === action.payload.id) {
-          item.compltetd = !item.compltetd;
-        }
+      state.value.push({
+        _id: state.value.length,
+        name: action.payload.name,
+        completed: false,
       });
     },
+
+    toggleTodo: (state, action) => {
+      state.value = state.value.map((item) => {
+        if (item._id === action.payload._id) {
+          item.completed = !item.completed;
+        }
+        return item;
+      });
+    },
+
     deleteTodo: (state, action) => {
-      state.value = state.value.filter((item) => {
-        return item.id !== action.payload.id;
+      state.value = state.value.filter(
+        (item) => item._id !== action.payload._id
+      );
+    },
+
+    updateTodo: (state, action) => {
+      state.value = state.value.map((item) => {
+        if (item._id === action.payload._id) {
+          item.name = action.payload.name;
+        }
+        return item;
       });
     },
   },
 });
 
-export const { addTodo, toggleTodo, deleteTodo } = todosSlice.actions;
+export const { addTodo, toggleTodo, deleteTodo, updateTodo } =
+  todosSlice.actions;
 export default todosSlice.reducer;
